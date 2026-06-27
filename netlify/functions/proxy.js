6d2deedc5a7c47b4784432c2ddcccd3227395d9b
Sormarkar <142065815+Sormarkar@users.noreply.github.com>
2026-06-27 19:07:08 +0800

    proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,29 +1,30 @@
-const cache = new Map();
-const CACHE_TTL = 60000;
+exports.handler = async (event) => {
 
-export default async (req) => {
-  const urlPath = req.url.split("/api/proxy/")[1] || "";
+  const urlPath = event.path.replace("/api/proxy/", "");
 
   const baseUrl = "https://ucdn.starhubgo.com/bpk-tv/HubSensasiHD/output/manifest.mpd";
-  const targetUrl = baseUrl + urlPath;
 
-  const cached = cache.get(targetUrl);
+  // kalau kau nak ignore path sepenuhnya:
+  const targetUrl = baseUrl;
 
-  if (cached && Date.now() - cached.time < CACHE_TTL) {
-    return new Response(cached.data, {
-      headers: { "X-Cache": "HIT" }
-    });
+  try {
+    const res = await fetch(targetUrl);
+
+    const data = await res.text();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/dash+xml",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: data
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: "Proxy error"
+    };
   }
-
-  const res = await fetch(targetUrl);
-  const data = await res.text();
-
-  cache.set(targetUrl, {
-    data,
-    time: Date.now()
-  });
-
-  return new Response(data, {
-    headers: { "X-Cache": "MISS" }
-  });
 };
