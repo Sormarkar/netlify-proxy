c029e0e123d5ccca72f97735f991b52ab9bde3e8
Sormarkar <142065815+Sormarkar@users.noreply.github.com>
2026-06-27 22:32:33 +0800

    proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,15 +1,13 @@
 exports.handler = async (event) => {
-  const mpdUrl =
-    "https://ucdn.starhubgo.com/bpk-tv/HubSensasiHD/output/manifest.mpd";
+  const target = event.queryStringParameters.url;
 
-  const res = await fetch(mpdUrl);
+  const res = await fetch(target);
   const text = await res.text();
 
   return {
     statusCode: res.status,
     headers: {
-      "content-type":
-        res.headers.get("content-type") || "application/dash+xml",
+      "content-type": "application/dash+xml",
       "access-control-allow-origin": "*",
     },
     body: text,
