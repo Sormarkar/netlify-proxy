a0cc3cf126bdbf4a4b2ab7305c8290d7aced2a6a
Sormarkar <142065815+Sormarkar@users.noreply.github.com>
2026-06-27 22:12:04 +0800

    proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,24 +1,17 @@
-module.exports = async (req) => {
-  try {
-    const mpdUrl =
-      "https://ucdn.starhubgo.com/bpk-tv/HubSensasiHD/output/manifest.mpd";
+exports.handler = async (event) => {
+  const mpdUrl =
+    "https://ucdn.starhubgo.com/bpk-tv/HubSensasiHD/output/manifest.mpd";
 
-    const res = await fetch(mpdUrl);
-    const text = await res.text();
+  const res = await fetch(mpdUrl);
+  const text = await res.text();
 
-    return {
-      statusCode: res.status,
-      headers: {
-        "content-type":
-          res.headers.get("content-type") || "application/dash+xml",
-        "access-control-allow-origin": "*",
-      },
-      body: text,
-    };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: err.message,
-    };
-  }
+  return {
+    statusCode: res.status,
+    headers: {
+      "content-type":
+        res.headers.get("content-type") || "application/dash+xml",
+      "access-control-allow-origin": "*",
+    },
+    body: text,
+  };
 };
