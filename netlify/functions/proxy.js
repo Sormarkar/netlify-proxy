8d960622c9688e79c501220d483b8fd3e6cb6abd
Sormarkar <142065815+Sormarkar@users.noreply.github.com>
2026-06-27 22:34:59 +0800

    proxy.js
    Refactor proxy function to handle errors and use optional chaining.

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,15 +1,23 @@
 exports.handler = async (event) => {
-  const target = event.queryStringParameters.url;
+  try {
+    const url = event.queryStringParameters?.url;
 
-  const res = await fetch(target);
-  const text = await res.text();
+    const res = await fetch(url);
+    const text = await res.text();
 
-  return {
-    statusCode: res.status,
-    headers: {
-      "content-type": "application/dash+xml",
-      "access-control-allow-origin": "*",
-    },
-    body: text,
-  };
+    return {
+      statusCode: res.status,
+      headers: {
+        "content-type":
+          res.headers.get("content-type") || "application/dash+xml",
+        "access-control-allow-origin": "*",
+      },
+      body: text,
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: err.message || "error",
+    };
+  }
 };
